2a3ffa516736f2c6912fa08a8879dc93a1c3cf80
suyog <suyogbankar01@gmail.com>
2019-09-07 22:47:57 +0530

    final commit

--- src/app/input-form/input-form.component.ts
+++ src/app/input-form/input-form.component.ts
@@ -36,48 +36,75 @@ export class InputFormComponent implements OnInit {
   ngOnInit() {
     // this.inputSubmit();
   }
-
-  inputSubmit(){
-    // alert('function call');
-    console.log(this.inputFieldForm.value);
-    // localStorage.removeItem('allInputField');
-    var previousInputData = JSON.parse(localStorage.getItem('allInputField')) || [];
-   
-    var count = previousInputData.length;
-
-    if(count < 1)
-    {
-      var id = 1;
+  onCheck(){
+    // alert('btn working')    
+    if(this.inputFieldForm.value.text === ''){
+      this.toastr.error('Text field cannot be empty','Sorry', {timeOut: 1500})
+    }else if(this.inputFieldForm.value.email == ''){
+      this.toastr.error('Email cannot be empty','Sorry', {timeOut: 1500})
+    }else if(this.inputFieldForm.value.password == ''){
+      this.toastr.error('Password cannot be empty','Sorry', {timeOut: 1500})
+    }else if(this.inputFieldForm.value.date == ''){
+      this.toastr.error('Date cannot be empty','Sorry', {timeOut: 1500})
+    }else if(this.inputFieldForm.value.file == ''){
+      this.toastr.error('File should be selected','Sorry', {timeOut: 1500})
+    }else if(this.inputFieldForm.value.url == ''){
+      this.toastr.error('Url cannot be empty','Sorry', {timeOut: 1500})
+    }else if(this.inputFieldForm.value.number == ''){
+      this.toastr.error('Number cannot be empty','Sorry', {timeOut: 1500})
+    }else if(this.inputFieldForm.value.tel == ''){
+      this.toastr.error('Tel cannot be empty','Sorry', {timeOut: 1500})
+    }else if(this.inputFieldForm.value.datetime == ''){
+      this.toastr.error('Datetime cannot be empty','Sorry', {timeOut: 1500})
+    }else if(this.inputFieldForm.value.month == ''){
+      this.toastr.error('Month shuld be selected','Sorry', {timeOut: 1500})
+    }else if(this.inputFieldForm.value.time == ''){
+      this.toastr.error('Time cannot be empty','Sorry', {timeOut: 1500})
+    }else if(this.inputFieldForm.value.week == ''){
+      this.toastr.error('Week cannot be empty','Sorry', {timeOut: 1500})
+    }else if(this.inputFieldForm.value.textarea == ''){
+      this.toastr.error('Textarea cannot be empty','Sorry', {timeOut: 1500})
+    }else if(this.inputFieldForm.value.dropdown == ''){
+      this.toastr.error('Country should be selected','Sorry', {timeOut: 1500})
+    }else if(this.inputFieldForm.value.gender == ''){
+      this.toastr.error('Gender should be selected','Sorry', {timeOut: 1500})
+    }else if(this.inputFieldForm.value.vehicles == ''){
+      this.toastr.error('Vechicles should be selected','Sorry', {timeOut: 1500})
     }else{
-      var id = ++count;
+      // else block starts here
+      var previousInputData = JSON.parse(localStorage.getItem('allInputField')) || [];
+      var count = previousInputData.length;
+        if(count < 1)
+        {
+          var id = 1;
+        }else{
+          var id = ++count;
+        }
+        var inputdata = {
+          'id' : id,
+          'text' : this.inputFieldForm.value.text,
+          'textarea' : this.inputFieldForm.value.textarea,
+          'email' : this.inputFieldForm.value.email,
+          'password' : this.inputFieldForm.value.password,
+          'date' : this.inputFieldForm.value.date,
+          'datetime' : this.inputFieldForm.value.datetime,
+          'month' : this.inputFieldForm.value.month,
+          'time' : this.inputFieldForm.value.time,
+          'week' : this.inputFieldForm.value.week,
+          'file' : this.inputFieldForm.value.file,
+          'url' : this.inputFieldForm.value.url,
+          'number' : this.inputFieldForm.value.number,
+          'tel' : this.inputFieldForm.value.tel,
+          'gender' : this.inputFieldForm.value.gender,
+          'vehicles' : this.inputFieldForm.value.vehicles,
+          'dropdown' : this.inputFieldForm.value.dropdown,
+        }
+        previousInputData.push(inputdata);
+        this.toastr.success('Your data has been registered.','Congradulations', {timeOut: 1500});
+        this.allInputData = localStorage.setItem('allInputField', JSON.stringify(previousInputData));
+        this.inputFieldForm.reset();
+        console.log('all data', this.allInputData);
+        // else block ends here    
     }
-
-    var inputdata = {
-      'id' : id,
-      'text' : this.inputFieldForm.value.text,
-      'textarea' : this.inputFieldForm.value.textarea,
-      'email' : this.inputFieldForm.value.email,
-      'password' : this.inputFieldForm.value.password,
-      'date' : this.inputFieldForm.value.date,
-      'datetime' : this.inputFieldForm.value.datetime,
-      'month' : this.inputFieldForm.value.month,
-      'time' : this.inputFieldForm.value.time,
-      'week' : this.inputFieldForm.value.week,
-      'file' : this.inputFieldForm.value.file,
-      'url' : this.inputFieldForm.value.url,
-      'number' : this.inputFieldForm.value.number,
-      'tel' : this.inputFieldForm.value.tel,
-      'gender' : this.inputFieldForm.value.gender,
-      'vehicles' : this.inputFieldForm.value.vehicles,
-      'dropdown' : this.inputFieldForm.value.dropdown,
-    }
-    previousInputData.push(inputdata);
-    this.toastr.success('Your data has been registered.','Congradulations', {timeOut: 1500});
-
-   this.allInputData = localStorage.setItem('allInputField', JSON.stringify(previousInputData));
-    this.inputFieldForm.reset();
-    console.log('all data', this.allInputData);
   }
-  
-
 }
